2e2856a2cd4ca0a93f59b3309e6876336024a419
Joe <1264204425@qq.com>
2023-09-26 20:30:49 +0800

    first commit
    first commit
    
    first commit
    
    first commit

---
+++ components/MusicLogo.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// @ts-ignore
+export const AcmeLogo = () => (
+    <>
+        <svg width="30" height="30" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M24 6V35" stroke="#333" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+            <path
+                d="M10 36.04C10 33.2565 12.2565 31 15.04 31H24V36.96C24 39.7435 21.7435 42 18.96 42H15.04C12.2565 42 10 39.7435 10 36.96V36.04Z"
+                fill="#2F88FF" stroke="#333" strokeWidth="4" strokeLinejoin="round"/>
+            <path fillRule="evenodd" clipRule="evenodd" d="M24 14.0664L36.8834 17.1215V9.01341L24 6.00002V14.0664Z"
+                  stroke="#333" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+    </>
+);
+
