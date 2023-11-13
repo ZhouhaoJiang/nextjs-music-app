1a5a0e5bfb41480b554cbad44967308f72279eb5
Joe <1264204425@qq.com>
2023-11-13 18:13:12 +0800

    fix: add live2d

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
-import React from "react";
-import { NextUIProvider } from "@nextui-org/react";
+import React, { useEffect } from "react";
+import { divider, NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { fontSans, fontMono } from "@/config/fonts";
 import type { AppProps } from "next/app";
@@ -8,6 +8,20 @@ import store from "@/redux/store.tsx";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        // 创建 script 标签
+        const script = document.createElement('script');
+        script.src = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js";
+        script.async = true;
+
+        // 将 script 标签添加到页面
+        document.body.appendChild(script);
+
+        // 清理函数
+        return () => {
+            document.body.removeChild(script);
+        }
+    }, []); // 空数组表示这个 effect 只在组件挂载时执行一次
     return (
         <NextUIProvider>
             <NextThemesProvider>
